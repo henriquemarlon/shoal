0200747721ddc7b80938dddeff1e24b71787f20f
Henrique Santos <henriquemarloncs@gmail.com>
2025-07-05 01:05:34 +0200

    feat(usecase): :bookmark: social account usecase

---
+++ internal/infra/usecase/social_account/create_social_account.go
@@ -0,0 +1,65 @@
+package social_account
+
+import (
+	"context"
+
+	"github.com/henriquemarlon/shoal/internal/domain/entity"
+	"github.com/henriquemarlon/shoal/internal/infra/repository"
+	"github.com/henriquemarlon/shoal/pkg/custom_type"
+	"github.com/rollmelette/rollmelette"
+)
+
+type CreateSocialAccountInputDTO struct {
+	Address  custom_type.Address `json:"address" validate:"required"`
+	Username string              `json:"username" validate:"required"`
+	Platform string              `json:"platform" validate:"required"`
+}
+
+type CreateSocialAccountOutputDTO struct {
+	Id        uint   `json:"id"`
+	UserId    uint   `json:"user_id"`
+	Username  string `json:"username"`
+	Platform  string `json:"platform"`
+	CreatedAt int64  `json:"created_at"`
+}
+
+type CreateSocialAccountUseCase struct {
+	UserRepository          repository.UserRepository
+	SocialAccountRepository repository.SocialAccountRepository
+}
+
+func NewCreateSocialAccountUseCase(userRepository repository.UserRepository, socialAccountRepository repository.SocialAccountRepository) *CreateSocialAccountUseCase {
+	return &CreateSocialAccountUseCase{
+		UserRepository:          userRepository,
+		SocialAccountRepository: socialAccountRepository,
+	}
+}
+
+func (s *CreateSocialAccountUseCase) Execute(ctx context.Context, input *CreateSocialAccountInputDTO, metadata *rollmelette.Metadata) (*CreateSocialAccountOutputDTO, error) {
+	user, err := s.UserRepository.FindUserByAddress(ctx, input.Address)
+	if err != nil {
+		return nil, err
+	}
+
+	socialAccount, err := entity.NewSocialAccount(
+		user.Id,
+		input.Username,
+		input.Platform,
+		metadata.BlockTimestamp,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	socialAccount, err = s.SocialAccountRepository.CreateSocialAccount(ctx, socialAccount)
+	if err != nil {
+		return nil, err
+	}
+	return &CreateSocialAccountOutputDTO{
+		Id:        socialAccount.Id,
+		UserId:    socialAccount.UserId,
+		Username:  socialAccount.Username,
+		Platform:  string(socialAccount.Platform),
+		CreatedAt: socialAccount.CreatedAt,
+	}, nil
+}
---
+++ internal/infra/usecase/social_account/find_social_account_by_id_.go
@@ -0,0 +1,36 @@
+package social_account
+
+import (
+	"context"
+
+	"github.com/henriquemarlon/shoal/internal/infra/repository"
+)
+
+type FindSocialAccountByIdInputDTO struct {
+	SocialAccountId uint `json:"social_account_id" validate:"required"`
+}
+
+type FindSocialAccountByIdUseCase struct {
+	SocialAccountRepository repository.SocialAccountRepository
+}
+
+func NewFindSocialAccountByIdUseCase(socialAccountRepository repository.SocialAccountRepository) *FindSocialAccountByIdUseCase {
+	return &FindSocialAccountByIdUseCase{
+		SocialAccountRepository: socialAccountRepository,
+	}
+}
+
+func (s *FindSocialAccountByIdUseCase) Execute(ctx context.Context, input *FindSocialAccountByIdInputDTO) (*FindSocialAccountOutputDTO, error) {
+	socialAccount, err := s.SocialAccountRepository.FindSocialAccountById(ctx, input.SocialAccountId)
+	if err != nil {
+		return nil, err
+	}
+	return &FindSocialAccountOutputDTO{
+		Id:        socialAccount.Id,
+		UserId:    socialAccount.UserId,
+		Username:  socialAccount.Username,
+		Platform:  string(socialAccount.Platform),
+		CreatedAt: socialAccount.CreatedAt,
+		UpdatedAt: socialAccount.UpdatedAt,
+	}, nil
+}
