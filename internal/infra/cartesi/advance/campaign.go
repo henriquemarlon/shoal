80876cf7ca5713ab0c43e4c43b02fa04a1dea3f9
Henrique Santos <henriquemarloncs@gmail.com>
2025-07-05 01:54:54 +0200

    feat: add advance handlers

---
+++ internal/infra/cartesi/advance/campaign.go
@@ -0,0 +1,265 @@
+package advance
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"math/big"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/go-playground/validator/v10"
+	"github.com/henriquemarlon/shoal/internal/domain/entity"
+	"github.com/henriquemarlon/shoal/internal/infra/repository"
+	"github.com/henriquemarlon/shoal/internal/usecase/campaign"
+	"github.com/holiman/uint256"
+	"github.com/rollmelette/rollmelette"
+)
+
+type CampaignAdvanceHandlers struct {
+	OrderRepository    repository.OrderRepository
+	UserRepository     repository.UserRepository
+	CampaignRepository repository.CampaignRepository
+}
+
+func NewCampaignAdvanceHandlers(
+	orderRepository repository.OrderRepository,
+	userRepository repository.UserRepository,
+	campaignRepository repository.CampaignRepository,
+) *CampaignAdvanceHandlers {
+	return &CampaignAdvanceHandlers{
+		OrderRepository:    orderRepository,
+		UserRepository:     userRepository,
+		CampaignRepository: campaignRepository,
+	}
+}
+
+func (h *CampaignAdvanceHandlers) CreateCampaign(env rollmelette.Env, metadata rollmelette.Metadata, deposit rollmelette.Deposit, payload []byte) error {
+	var input campaign.CreateCampaignInputDTO
+	if err := json.Unmarshal(payload, &input); err != nil {
+		return fmt.Errorf("failed to unmarshal input: %w", err)
+	}
+
+	validator := validator.New()
+	if err := validator.Struct(input); err != nil {
+		return fmt.Errorf("failed to validate input: %w", err)
+	}
+
+	ctx := context.Background()
+	createCampaign := campaign.NewCreateCampaignUseCase(
+		h.CampaignRepository,
+		h.UserRepository,
+	)
+
+	res, err := createCampaign.Execute(ctx, &input, deposit, metadata)
+	if err != nil {
+		return fmt.Errorf("failed to create campaign: %w", err)
+	}
+
+	erc20Deposit := deposit.(*rollmelette.ERC20Deposit)
+	if err := env.ERC20Transfer(
+		erc20Deposit.Token,
+		erc20Deposit.Sender,
+		env.AppAddress(),
+		erc20Deposit.Value,
+	); err != nil {
+		return fmt.Errorf("failed to transfer ERC20: %w", err)
+	}
+
+	campaign, err := json.Marshal(res)
+	if err != nil {
+		return fmt.Errorf("failed to marshal response: %w", err)
+	}
+
+	env.Notice(append([]byte("campaign created - "), campaign...))
+	return nil
+}
+
+func (h *CampaignAdvanceHandlers) CloseCampaign(env rollmelette.Env, metadata rollmelette.Metadata, deposit rollmelette.Deposit, payload []byte) error {
+	var input campaign.CloseCampaignInputDTO
+	if err := json.Unmarshal(payload, &input); err != nil {
+		return fmt.Errorf("failed to unmarshal input: %w", err)
+	}
+
+	validator := validator.New()
+	if err := validator.Struct(input); err != nil {
+		return fmt.Errorf("failed to validate input: %w", err)
+	}
+
+	ctx := context.Background()
+	closeCampaign := campaign.NewCloseCampaignUseCase(h.CampaignRepository, h.OrderRepository)
+	res, err := closeCampaign.Execute(ctx, &input, metadata)
+	if err != nil && res == nil {
+		return fmt.Errorf("failed to close campaign: %w", err)
+	}
+
+	token := common.Address(res.Token)
+
+	// Process orders
+	for _, order := range res.Orders {
+		if order.State == entity.OrderStateRejected {
+			if err = env.ERC20Transfer(
+				token,
+				env.AppAddress(),
+				common.Address(order.Investor),
+				order.Amount.ToBig(),
+			); err != nil {
+				return fmt.Errorf("failed to transfer rejected order: %w", err)
+			}
+		}
+	}
+
+	abiJSON := `[{
+		"type":"function",
+		"name":"mint",
+		"inputs":[
+			{"type":"uint64"},
+			{"type":"address"},
+			{"type":"address"}
+		]
+	}]`
+
+	abiInterface, err := abi.JSON(strings.NewReader(abiJSON))
+	if err != nil {
+		return fmt.Errorf("failed to parse ABI: %w", err)
+	}
+
+	for _, order := range res.Orders {
+		if order.State != entity.OrderStateRejected {
+			voucher, err := abiInterface.Pack(
+				"mint",
+				order.BadgeChainId,
+				common.Address(order.Investor),
+				common.Address(res.BadgeMinter),
+			)
+			if err != nil {
+				return fmt.Errorf("failed to pack ABI: %w", err)
+			}
+			env.Voucher(common.Address(res.BadgeRouter), big.NewInt(0), voucher)
+		}
+	}
+
+	if err := env.ERC20Transfer(token, env.AppAddress(), common.Address(res.Creator), res.TotalRaised.ToBig()); err != nil {
+		return fmt.Errorf("failed to transfer total raised: %w", err)
+	}
+
+	campaign, err := json.Marshal(res)
+	if err != nil {
+		return fmt.Errorf("failed to marshal response: %w", err)
+	}
+
+	env.Notice(append([]byte(fmt.Sprintf("campaign %v - ", res.State)), campaign...))
+	return nil
+}
+
+func (h *CampaignAdvanceHandlers) SettleCampaign(env rollmelette.Env, metadata rollmelette.Metadata, deposit rollmelette.Deposit, payload []byte) error {
+	var input campaign.SettleCampaignInputDTO
+	if err := json.Unmarshal(payload, &input); err != nil {
+		return fmt.Errorf("failed to unmarshal input: %w", err)
+	}
+
+	validator := validator.New()
+	if err := validator.Struct(input); err != nil {
+		return fmt.Errorf("failed to validate input: %w", err)
+	}
+
+	ctx := context.Background()
+	settleCampaign := campaign.NewSettleCampaignUseCase(
+		h.CampaignRepository,
+		h.OrderRepository,
+	)
+
+	res, err := settleCampaign.Execute(ctx, &input, deposit, metadata)
+	if err != nil {
+		return fmt.Errorf("failed to settle campaign: %w", err)
+	}
+
+	contractAddr := common.Address(res.Token)
+	creatorAddr := common.Address(res.Creator)
+
+	// Process settled orders
+	for _, order := range res.Orders {
+		if order.State == entity.OrderStateSettled {
+			// Calculate interest for this order
+			interest := new(uint256.Int).Mul(order.Amount, order.InterestRate)
+			interest.Div(interest, uint256.NewInt(100))
+
+			// Calculate total payment
+			totalPayment := new(uint256.Int).Add(order.Amount, interest)
+
+			if err := env.ERC20Transfer(
+				contractAddr,
+				creatorAddr,
+				common.Address(order.Investor),
+				totalPayment.ToBig(),
+			); err != nil {
+				return fmt.Errorf("failed to transfer settled order: %w", err)
+			}
+		}
+	}
+
+	campaign, err := json.Marshal(res)
+	if err != nil {
+		return fmt.Errorf("failed to marshal response: %w", err)
+	}
+
+	env.Notice(append([]byte("campaign settled - "), campaign...))
+	return nil
+}
+
+func (h *CampaignAdvanceHandlers) ExecuteCampaignCollateral(env rollmelette.Env, metadata rollmelette.Metadata, deposit rollmelette.Deposit, payload []byte) error {
+	var input campaign.ExecuteCampaignCollateralInputDTO
+	if err := json.Unmarshal(payload, &input); err != nil {
+		return fmt.Errorf("failed to unmarshal input: %w", err)
+	}
+
+	validator := validator.New()
+	if err := validator.Struct(input); err != nil {
+		return fmt.Errorf("failed to validate input: %w", err)
+	}
+
+	ctx := context.Background()
+	executeCampaignCollateral := campaign.NewExecuteCampaignCollateralUseCase(h.CampaignRepository, h.OrderRepository)
+	res, err := executeCampaignCollateral.Execute(ctx, &input, metadata)
+	if err != nil {
+		return fmt.Errorf("failed to execute campaign collateral: %w", err)
+	}
+
+	totalFinalValue := uint256.NewInt(0)
+	orderFinalValues := make(map[uint]*uint256.Int)
+	for _, order := range res.Orders {
+		if order.State == entity.OrderStateSettledByCollateral {
+			interest := new(uint256.Int).Mul(order.Amount, order.InterestRate)
+			interest.Div(interest, uint256.NewInt(100))
+			finalValue := new(uint256.Int).Add(order.Amount, interest)
+			orderFinalValues[order.Id] = finalValue
+			totalFinalValue.Add(totalFinalValue, finalValue)
+		}
+	}
+
+	for _, order := range res.Orders {
+		if order.State == entity.OrderStateSettledByCollateral {
+			finalValue := orderFinalValues[order.Id]
+			orderShare := new(uint256.Int).Mul(finalValue, res.CollateralAmount)
+			orderShare.Div(orderShare, totalFinalValue)
+
+			if err = env.ERC20Transfer(
+				common.Address(res.CollateralAddress),
+				env.AppAddress(),
+				common.Address(order.Investor),
+				orderShare.ToBig(),
+			); err != nil {
+				return fmt.Errorf("failed to transfer collateral to investor: %w", err)
+			}
+		}
+	}
+
+	campaign, err := json.Marshal(res)
+	if err != nil {
+		return fmt.Errorf("failed to marshal response: %w", err)
+	}
+
+	env.Notice(append([]byte("campaign collateral executed - "), campaign...))
+	return nil
+}
---
+++ internal/infra/cartesi/advance/social_account.go
@@ -0,0 +1,76 @@
+package advance
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/henriquemarlon/shoal/internal/infra/repository"
+	"github.com/henriquemarlon/shoal/internal/usecase/social_account"
+	"github.com/rollmelette/rollmelette"
+)
+
+type SocialAccountAdvanceHandlers struct {
+	UserRepository          repository.UserRepository
+	SocialAccountRepository repository.SocialAccountRepository
+}
+
+func NewSocialAccountAdvanceHandlers(userRepository repository.UserRepository, socialAccountRepository repository.SocialAccountRepository) *SocialAccountAdvanceHandlers {
+	return &SocialAccountAdvanceHandlers{
+		UserRepository:          userRepository,
+		SocialAccountRepository: socialAccountRepository,
+	}
+}
+
+func (s *SocialAccountAdvanceHandlers) CreateSocialAccount(env rollmelette.Env, metadata rollmelette.Metadata, deposit rollmelette.Deposit, payload []byte) error {
+	var input social_account.CreateSocialAccountInputDTO
+	err := json.Unmarshal(payload, &input)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal payload: %w", err)
+	}
+
+	validator := validator.New()
+	if err := validator.Struct(input); err != nil {
+		return fmt.Errorf("failed to validate input: %w", err)
+	}
+
+	ctx := context.Background()
+	createSocialAccount := social_account.NewCreateSocialAccountUseCase(s.UserRepository, s.SocialAccountRepository)
+	res, err := createSocialAccount.Execute(ctx, &input, &metadata)
+	if err != nil {
+		return err
+	}
+	socialAccount, err := json.Marshal(res)
+	if err != nil {
+		return err
+	}
+	env.Notice(append([]byte("social account created - "), socialAccount...))
+	return nil
+}
+
+func (s *SocialAccountAdvanceHandlers) DeleteSocialAccount(env rollmelette.Env, metadata rollmelette.Metadata, deposit rollmelette.Deposit, payload []byte) error {
+	var input social_account.DeleteSocialAccountInputDTO
+	err := json.Unmarshal(payload, &input)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal payload: %w", err)
+	}
+
+	validator := validator.New()
+	if err := validator.Struct(input); err != nil {
+		return fmt.Errorf("failed to validate input: %w", err)
+	}
+
+	ctx := context.Background()
+	deleteSocialAccount := social_account.NewDeleteSocialAccountUseCase(s.SocialAccountRepository)
+	err = deleteSocialAccount.Execute(ctx, &input)
+	if err != nil {
+		return err
+	}
+	socialAccount, err := json.Marshal(input)
+	if err != nil {
+		return err
+	}
+	env.Notice(append([]byte("social account deleted - "), socialAccount...))
+	return nil
+}
