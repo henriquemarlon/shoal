6c67dcf6540e24fcdb0d5fe893661bd84bcf259e
Henrique Santos <henriquemarloncs@gmail.com>
2025-07-05 01:41:39 +0200

    feat: add router for cartesi inputs

---
+++ internal/infra/cartesi/middleware/rbac.go
@@ -0,0 +1,86 @@
+package middleware
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/henriquemarlon/shoal/internal/infra/repository"
+	"github.com/henriquemarlon/shoal/internal/usecase/user"
+	"github.com/henriquemarlon/shoal/pkg/custom_type"
+	"github.com/henriquemarlon/shoal/pkg/router"
+	"github.com/rollmelette/rollmelette"
+)
+
+type RBACFactory struct {
+	UserRepository repository.UserRepository
+}
+
+func NewRBACFactory(userRepository repository.UserRepository) *RBACFactory {
+	return &RBACFactory{
+		UserRepository: userRepository,
+	}
+}
+
+func (f *RBACFactory) Create(roles []string) router.Middleware {
+	return func(handler any) any {
+		switch h := handler.(type) {
+		case router.AdvanceHandlerFunc:
+			return router.AdvanceHandlerFunc(func(env rollmelette.Env, metadata rollmelette.Metadata, deposit rollmelette.Deposit, payload []byte) error {
+				var address custom_type.Address
+				ctx := context.Background()
+
+				// Get the sender address from either ERC20 deposit or metadata
+				erc20Deposit, ok := deposit.(*rollmelette.ERC20Deposit)
+				if ok {
+					address = custom_type.Address(erc20Deposit.Sender)
+				} else {
+					address = custom_type.Address(metadata.MsgSender)
+				}
+
+				// Find user and check roles
+				findUserByAddress := user.NewFindUserByAddressUseCase(f.UserRepository)
+				user, err := findUserByAddress.Execute(ctx, &user.FindUserByAddressInputDTO{
+					Address: address,
+				})
+				if err != nil {
+					return err
+				}
+
+				// Check if user has any of the required roles
+				var hasRole bool
+				for _, role := range roles {
+					if user.Role == role {
+						hasRole = true
+						break
+					}
+				}
+				if !hasRole {
+					return fmt.Errorf("user %s lacks required permissions: %v", common.Address(user.Address), roles)
+				}
+
+				return h(env, metadata, deposit, payload)
+			})
+		case router.InspectHandlerFunc:
+			return h
+		default:
+			return handler
+		}
+	}
+}
+
+func (f *RBACFactory) AdminOnly() router.Middleware {
+	return f.Create([]string{"admin"})
+}
+
+func (f *RBACFactory) VerifierOnly() router.Middleware {
+	return f.Create([]string{"verifier"})
+}
+
+func (f *RBACFactory) InvestorOnly() router.Middleware {
+	return f.Create([]string{"investor"})
+}
+
+func (f *RBACFactory) CreatorOnly() router.Middleware {
+	return f.Create([]string{"creator"})
+}
