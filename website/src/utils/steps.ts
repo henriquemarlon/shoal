1c2d604d4e4c46e633320e8699d436cf840f4bc8
Emanuele Morais <emanuele.lmorais@gmail.com>
2025-07-05 03:15:27 +0200

    feat: add utility functions for Ethereum faucet, browser extension checks, and step management for NFT minting process

---
+++ website/src/utils/steps.ts
@@ -0,0 +1,77 @@
+import {
+    MintStep,
+    ProveStep,
+    SuccessStep,
+    WelcomeScreen,
+    InstallExtension,
+  } from "../components/vlayer";
+  
+  export type Step = {
+    kind: STEP_KIND;
+    path: string;
+    backUrl?: string;
+    component: React.ComponentType;
+    title: string;
+    description: string;
+    headerIcon?: string;
+    index: number;
+  };
+  
+  export enum STEP_KIND {
+    WELCOME,
+    CONNECT_WALLET,
+    START_PROVING,
+    MINT,
+    INSTALL_EXTENSION,
+    SUCCESS,
+  }
+  export const steps: Step[] = [
+    {
+      path: "",
+      kind: STEP_KIND.WELCOME,
+      component: WelcomeScreen,
+      title: "X NFT",
+      description:
+        "Mint an NFT with your X account. Only owner of account can mint NFT for specific handle. This example demonstrates use of Web Proofs.",
+      headerIcon: "/nft-illustration.svg",
+      index: 0,
+    },
+    {
+      path: "start-proving",
+      kind: STEP_KIND.START_PROVING,
+      backUrl: "/",
+      component: ProveStep,
+      title: "X NFT",
+      description:
+        "Open vlayer browser extension and follow instructions in order to produce the Proof of X account ownership. \n",
+      index: 1,
+    },
+    {
+      path: "install-extension",
+      kind: STEP_KIND.INSTALL_EXTENSION,
+      component: InstallExtension,
+      backUrl: "/",
+      title: "X NFT",
+      description: `Install vlayer browser extension to proceed to the next step. \n`,
+      index: 2,
+    },
+    {
+      path: "mint",
+      kind: STEP_KIND.MINT,
+      backUrl: "/start-proving",
+      component: MintStep,
+      title: "X NFT",
+      description: `You are all set to mint your unique X NFT, a true reflection of your verified identity.`,
+      index: 3,
+    },
+    {
+      path: "success",
+      kind: STEP_KIND.SUCCESS,
+      component: SuccessStep,
+      title: "Success",
+      description: "",
+      headerIcon: "/success-illustration.svg",
+      index: 4,
+    },
+  ];
+  
