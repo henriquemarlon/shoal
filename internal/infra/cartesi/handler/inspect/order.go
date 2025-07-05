93fdd5565efef58b6d165802fc2103b5785d36be
Henrique Santos <henriquemarloncs@gmail.com>
2025-07-05 02:01:11 +0200

    feat: add advance inpect handlers WIP

---
+++ internal/infra/cartesi/handler/inspect/order.go
@@ -0,0 +1,96 @@
+package inspect
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/henriquemarlon/shoal/internal/infra/repository"
+	"github.com/henriquemarlon/shoal/internal/usecase/order"
+	"github.com/rollmelette/rollmelette"
+)
+
+type OrderInspectHandlers struct {
+	OrderRepository repository.OrderRepository
+}
+
+func NewOrderInspectHandlers(orderRepository repository.OrderRepository) *OrderInspectHandlers {
+	return &OrderInspectHandlers{
+		OrderRepository: orderRepository,
+	}
+}
+
+func (h *OrderInspectHandlers) FindOrderById(env rollmelette.EnvInspector, payload []byte) error {
+	var input order.FindOrderByIdInputDTO
+	if err := json.Unmarshal(payload, &input); err != nil {
+		return fmt.Errorf("failed to unmarshal input: %w", err)
+	}
+
+	ctx := context.Background()
+	findOrderById := order.NewFindOrderByIdUseCase(h.OrderRepository)
+	res, err := findOrderById.Execute(ctx, &input)
+	if err != nil {
+		return fmt.Errorf("failed to find order: %w", err)
+	}
+	order, err := json.Marshal(res)
+	if err != nil {
+		return fmt.Errorf("failed to marshal order: %w", err)
+	}
+	env.Report(order)
+	return nil
+}
+
+func (h *OrderInspectHandlers) FindBidsByCampaignId(env rollmelette.EnvInspector, payload []byte) error {
+	var input order.FindOrdersByCampaignIdInputDTO
+	if err := json.Unmarshal(payload, &input); err != nil {
+		return fmt.Errorf("failed to unmarshal input: %w", err)
+	}
+
+	ctx := context.Background()
+	findOrdersByCampaignId := order.NewFindOrdersByCampaignIdUseCase(h.OrderRepository)
+	res, err := findOrdersByCampaignId.Execute(ctx, &input)
+	if err != nil {
+		return fmt.Errorf("failed to find orders by campaign id: %v", err)
+	}
+	orders, err := json.Marshal(res)
+	if err != nil {
+		return fmt.Errorf("failed to marshal orders: %w", err)
+	}
+	env.Report(orders)
+	return nil
+}
+
+func (h *OrderInspectHandlers) FindAllOrders(env rollmelette.EnvInspector, payload []byte) error {
+	ctx := context.Background()
+	findAllOrders := order.NewFindAllOrdersUseCase(h.OrderRepository)
+	res, err := findAllOrders.Execute(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to find all orders: %w", err)
+	}
+	allOrders, err := json.Marshal(res)
+	if err != nil {
+		return fmt.Errorf("failed to marshal all orders: %w", err)
+	}
+	env.Report(allOrders)
+	return nil
+}
+
+func (h *OrderInspectHandlers) FindOrdersByInvestor(env rollmelette.EnvInspector, payload []byte) error {
+	var input order.FindOrdersByInvestorInputDTO
+	if err := json.Unmarshal(payload, &input); err != nil {
+		return fmt.Errorf("failed to unmarshal input: %w", err)
+	}
+
+	ctx := context.Background()
+	findOrdersByInvestor := order.NewFindOrdersByInvestorUseCase(h.OrderRepository)
+	res, err := findOrdersByInvestor.Execute(ctx, &input)
+	if err != nil {
+		return fmt.Errorf("failed to find orders by investor: %w", err)
+	}
+	orders, err := json.Marshal(res)
+	if err != nil {
+		return fmt.Errorf("failed to marshal orders: %w", err)
+	}
+	env.Report(orders)
+	return nil
+}
---
+++ internal/infra/cartesi/handler/inspect/user.go
@@ -0,0 +1,97 @@
+package inspect
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/go-playground/validator/v10"
+	"github.com/henriquemarlon/shoal/internal/infra/repository"
+	"github.com/henriquemarlon/shoal/internal/usecase/user"
+	"github.com/rollmelette/rollmelette"
+)
+
+type UserInspectHandlers struct {
+	UserRepository repository.UserRepository
+}
+
+func NewUserInspectHandlers(userRepository repository.UserRepository) *UserInspectHandlers {
+	return &UserInspectHandlers{
+		UserRepository: userRepository,
+	}
+}
+
+func (h *UserInspectHandlers) FindUserByAddress(env rollmelette.EnvInspector, payload []byte) error {
+	var input user.FindUserByAddressInputDTO
+	if err := json.Unmarshal(payload, &input); err != nil {
+		return fmt.Errorf("failed to unmarshal input: %w", err)
+	}
+
+	validator := validator.New()
+	if err := validator.Struct(input); err != nil {
+		return fmt.Errorf("failed to validate input: %w", err)
+	}
+
+	ctx := context.Background()
+	findUserByAddress := user.NewFindUserByAddressUseCase(h.UserRepository)
+	res, err := findUserByAddress.Execute(ctx, &input)
+	if err != nil {
+		return fmt.Errorf("failed to find User: %w", err)
+	}
+	User, err := json.Marshal(res)
+	if err != nil {
+		return fmt.Errorf("failed to marshal User: %w", err)
+	}
+	env.Report(User)
+	return nil
+}
+
+func (h *UserInspectHandlers) FindAllUsers(env rollmelette.EnvInspector, payload []byte) error {
+	ctx := context.Background()
+	findAllUsers := user.NewFindAllUsersUseCase(h.UserRepository)
+	res, err := findAllUsers.Execute(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to find all Users: %w", err)
+	}
+	allUsers, err := json.Marshal(res)
+	if err != nil {
+		return fmt.Errorf("failed to marshal all Users: %w", err)
+	}
+	env.Report(allUsers)
+	return nil
+}
+
+func (h *UserInspectHandlers) ERC20BalanceOf(env rollmelette.EnvInspector, payload []byte) error {
+	var input user.BalanceOfInputDTO
+	if err := json.Unmarshal(payload, &input); err != nil {
+		return fmt.Errorf("failed to unmarshal input: %w", err)
+	}
+
+	validator := validator.New()
+	if err := validator.Struct(input); err != nil {
+		return fmt.Errorf("failed to validate input: %w", err)
+	}
+
+	ctx := context.Background()
+	findUserByAddress := user.NewFindUserByAddressUseCase(h.UserRepository)
+	res, err := findUserByAddress.Execute(ctx, &user.FindUserByAddressInputDTO{
+		Address: input.Address,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to find User: %w", err)
+	}
+
+	balance := env.ERC20BalanceOf(
+		common.Address(input.Token),
+		common.Address(res.Address),
+	).String()
+
+	balanceBytes, err := json.Marshal(balance)
+	if err != nil {
+		return fmt.Errorf("failed to marshal balance: %w", err)
+	}
+
+	env.Report(balanceBytes)
+	return nil
+}
